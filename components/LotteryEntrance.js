f288cd9ec58a247e783e3d147f29363f181ef876
robitu <robertoiturralde2009@hotmail.com>
2023-03-26 22:08:08 -0600

    Constants folder, added useWeb3Contract webhook

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,6 @@
+import { useWeb3Contract } from "react-moralis"
+
+export default function LotteryEntrance() {
+    /*const {runContractFunction: enterRaffle} = useWeb3Contract({abi:, contractAddress:, functionName:, params: {}, msgValue:})*/
+    return <div>Enter the lottery</div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head"
 import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
-import ManualHeader from "@/components/ManualHeader"
+//import ManualHeader from "@/components/ManualHeader"
 import { useMoralis } from "react-moralis"
-
-const inter = Inter({ subsets: ["latin"] })
+import Header from "@/components/Header"
+import LotteryEntrance from "../components/LotteryEntrance"
 
 export default function Home() {
     return (
@@ -15,7 +15,9 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <ManualHeader />
+            {/*<ManualHeader /> */}
+            <Header />
+            <LotteryEntrance />
             Hello!
         </>
     )
